cc5a9c08e249d79795ff372e4c3a293ac88d66ca
JoaPul <joaquinarroyo@outlook.com>
2022-03-06 22:40:12 -0600

    bottons changes

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const HMI = () => {
         border: 8px solid #4C5958;
         background-color: #8AA6A3;
         transition-property: background-color, border;
-        transition-duration: 3s, 2s;
+        transition-duration: 2s, 1s;
       }
       .header .yo {
         border: 8px solid #4C5958;
         transition-property: border;
-        transition-duration: 2s;
+        transition-duration: 1s;
       }
       .nombre {
         color: #4C5958;
         transition-property: color;
-        transition-duration: 2s;
+        transition-duration: 1s;
       }
     `;
 };
+
 const HMO = () => {
   document.getElementById("style").innerHTML = `
     .header {
@@ -69,6 +70,35 @@ const perf = () => {
     height: 100px;
   }
   `;
+
+  //CAMBIO DE ACCION CUANDO SE PASA EL MOUSE
   document.getElementById("header").setAttribute("onmouseover", "");
   document.getElementById("header").setAttribute("onmouseout", "");
+};
+
+//CAMBIO DE COLOR AL BOTON DE REGREASAR
+const Inicio = () => {
+  document.getElementById("perf").innerHTML = ``;
+  HMO();
+
+  //CAMBIO DE ACCION CUANDO SE PASA EL MOUSE
+  document.getElementById("header").setAttribute("onmouseover", "HMI()");
+  document.getElementById("header").setAttribute("onmouseout", "HMO()");
+};
+// onmouseover="InicioHMI()" onmouseout="InicioHMO()"
+const InicioHMI = () => {
+  document.getElementById("regre").setAttribute(
+    "style",
+    `
+    background-color: #0d574f;
+    border: 5px solid #afd3cf;
+    transition-property: background-color, border;
+    transition-duration: 2s, 1s;
+  }
+  `
+  );
+};
+
+const InicioHMO = () => {
+  document.getElementById("regre").setAttribute("style", "border: 5px solid #8aa6a3; background-color: #127369; transition-property: background-color, border; transition-duration: 2s, 1s;");
 };
